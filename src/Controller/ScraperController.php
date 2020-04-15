a01efb7ae02dd99f5310b31fe62aded964e2557d
OddIsBack-212 <damien.nicolleau2019@campus-eni.fr>
2020-04-15 19:21:56 +0200

    version initiale du projet

---
+++ src/Controller/ScraperController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ScraperController extends AbstractController
+{
+    /**
+     * @Route("/scraper", name="scraper")
+     */
+    public function index()
+    {
+
+        return $this->render('scraper/index.html.twig', [
+            'controller_name' => 'ScraperController','titre'=>'twitter'
+        ]);
+    }
+
+    public function GoogleSearch(String $keyWordUser){
+        $keyWordUser='twitter';
+        $ch=curl_init();//ouvre la connection
+        curl_setopt($ch,CURLOPT_URL,"https://www.google.com/search?q=".$keyWordUser);
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1); //ici la valeur 1 vaut true
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); //ici la valeur 1 vaut true
+        $response= curl_exec($ch);
+        curl_close($ch);//ferme la connection
+
+        echo $response;
+
+
+    }
+}
